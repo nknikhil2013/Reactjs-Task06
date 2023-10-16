86e383c3a07e0ec544179a91bd8faae068393059
nknikhil2013 <nknikhi2013@gmail.com>
2023-10-16 22:37:34 +0530

    first commit

---
+++ src/components/Navcomponents.js
@@ -0,0 +1,188 @@
+import './style.css';
+import  abs  from './images/abs.png';
+import download from './images/download.png';
+
+
+
+export function Home(){
+    return(
+        <section style={{padding:"10px 0px",backgroundColor: "rgb(110, 5, 238)"}} id="Home">
+            <div className="container-lg">
+                <div className="row justify-content-center bg-info">
+                    <div className="col-lg-10" id="back">
+                        <img src={abs} className="img-fluid" alt=""></img>
+                        <div className="text-center text-light" id="back-text">
+                            <h1>Name: Yash Jain</h1>
+                            <p>The website demonstrates my portfolio</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export function About(){
+    return(
+        <section id="intro" className='mt-2'>
+            <div class="container-lg">
+                <div class="row justify-content-center align-item-center">
+                    <div class="col-md-5 text-center text-md-start border border-secondary" style={{backgroundColor: "rgb(255, 255, 255)"}}>
+                        <h1>
+                            <div class="display-4">Yash Jain</div>
+                            <div class="lead text-muted"><i class="bi bi-file-earmark-person"></i> A computer science student</div>
+                        </h1>
+                        <hr />
+                        <div class="contact">
+                            <p class="lead pt-0">Currently learning AWS cloud practitioner skils and MERN-Stack</p>
+                            <span class="lead"><i class="bi bi-envelope-at-fill"></i> Contacts : 
+                                <a href="mailto:nknikhil2013@gmail.com" class="btn btn-sm btn-primary fw-bold">Email</a>
+                            </span>
+                            <br /><br />
+                            <span class="lead"><i class="bi bi-geo-fill"></i> Bangalore</span>
+                            <br /><br />
+                            <span class="lead fw-900"><i class="bi bi-at"></i> Connections</span>
+            
+                            <div class="d-block mt-3 btn btn-secondary">
+                                Explore more about me<i class="bi bi-chevron-double-right"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export function Info(){
+    return(
+        <section id="info" class="bg-light mt-5" style={{padding: "60px 0px"}}>
+            <div class="container-lg">
+                <div class="text-center">
+                    <h2><i class="bi bi-info-circle-fill d-none d-md-inline"></i> Info Check</h2>
+                    <p class="lead text-muted">More details...</p>
+                </div>
+                <div class="row my-5 aligh-items-center justify-content-center">
+                    <div class="col-8 col-lg-4 col-xl-3">
+                        <div class="card border-info border-2">
+                            <h4 class="card-header border-info text-center">Web Development</h4>
+                            <div class="card-body text-center py-4">
+                                <h5 class="card-title">All of my works in this domain</h5>
+                                <p class="card-text mx-3 my-1 text-muted d-none d-lg-block">MERN projects are useful towards web development and deployment</p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+
+
+export function Contact(){
+    return(
+        <section id="contact" style={{backgroundColor: "rgb(110, 5, 238)"}}>
+            <div class="container-lg">
+                <div class="text-center">
+                    <h2><i class="bi bi-hand-index-thumb-fill"></i> Get in Touch</h2>
+                    <p class="lead text-muted">Any queries? </p>
+                </div>
+
+                <div class="row justify-content-center my-5">
+                    <div class="col-lg-6">
+                        <form action="">
+                            <label for="email" class="form-label">Email address:</label>
+                            <div class="mb-4 input-group">
+                                <span class="input-group-text">
+                                    <i class="bi bi-envelope-fill"></i>
+                                </span>
+                                <input type="email" class="form-control" id="email" placeholder="e.g. xyz@email.com" required />
+
+                                <span class="input-group-text">
+                                    <span class="tt" data-bs-placement="bottom" title="Enter email">
+                                        <i class="bi bi-question-circle text-muted"></i>
+                                    </span>
+                                </span>
+                            </div>
+
+                            <label for="name" class="form-label">Name:</label>
+                            <div class="mb-4 input-group">
+                                <span class="input-group-text">
+                                    <i class="bi bi-person-fill"></i>
+                                </span>
+                                <input type="text" class="form-control" id="name" placeholder="e.g. Jacob" required/>
+
+                                <span class="input-group-text">
+                                    <span class="tt" data-bs-placement="bottom" title="Pretty self explanatory really...">
+                                        <i class="bi bi-question-circle text-muted"></i>
+                                    </span>
+                                </span>
+                            </div>
+
+                            <label for="subject" class="form-label">What queries do you have?</label>
+
+                            <div class="form-floating mb-4">
+                                <textarea class="form-control" id="query" rows="5"></textarea>
+                                <label for="query" class="form-label">Your query...</label>
+                            </div>
+
+                            <div class="mb-4 text-center">
+                                <button type="submit" class="btn btn-warning">Submit <i class="bi bi-send-fill"></i></button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    )
+}
+
+export function Project(){
+    return(
+        <section id="projects">
+            <div class="container-md">
+                <div class="text-center">
+                    <h2><i class="bi bi-person-workspace"></i> Main Project Works</h2>
+                    <p class="lead text-muted">A quick glance at my small project</p>
+                </div>
+
+                <div class="row my-5 g-5 justify-content-around align-items-center">
+                    <div class="col-6 col-lg-4">
+                        <img src={download} class="img-fluid" alt="" style={{width:"75%"}}/>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <div class="accordion" id="projects_list">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="heading-1">
+                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#project-1" aria-expanded="true" aria-controls="project-1">
+                                        RECURSIVE PARSER
+                                    </button>
+                                </h2>
+                                <div id="project-1" class="accordion-collapse collpase show" aria-labelledby="heading-1" data-bs-parent="#projects_list">
+                                    <div class="accordion-body">
+                                        <p>
+                                            This is a recursive parser used in compilers
+                                        </p>
+                            
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="heading-2">
+                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#project-2" aria-expanded="false" aria-controls="project-2">
+                                        
+                                    </button>
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
